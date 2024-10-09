fd7c23831be6a11bd7a492bb5b12b9c38d5ed077
filipomarchevski <filipomarchvski@gmail.com>
2024-10-09 07:48:17 +0300

    Extract the square dragging logic into a separate module

--- scripts/canvas/canvas-configuration.js
+++ scripts/canvas/canvas-configuration.js
@@ -1,92 +1,62 @@
-import appendSquareToCanvas from '../shape-factory/square-factory.js';
+import {
+  createSquare,
+  resetDragState,
+  resizeSquareWhileDragging,
+} from '../shape-drag-sizing/square-drag.js';
 
 const canvas = document.getElementById('canvas');
 
-let createShapeFunction;
-
-let shape, startX, startY;
-
-let isDragging = false;
+// Each shape implements differently the drag logic therefore we keep the functions for the 3 stages of the drag in this object.
+let dragFunctions;
 
 export default function configureCanvas() {
   const squareButton = document.getElementById('create-square-button');
+  configureShapeButtons();
+  configureShapeDragging();
+}
+
+function configureShapeButtons() {
   squareButton.addEventListener('click', () => {
-    if (!createShapeFunction) {
-      createShapeFunction = appendSquareToCanvas;
+    if (!dragFunctions) {
+      dragFunctions = {
+        onMouseDown: createSquare,
+        onMouseMove: resizeSquareWhileDragging,
+        onMouseUp: resetDragState,
+      };
     } else {
-      createShapeFunction = null;
+      dragFunctions = null;
     }
   });
-  configureShapeDragging();
 }
 
 // When we want to create a shape it should be done by clicking on the corresponding shape button
 // and then dragging the size of the shape on the canvas.
 function configureShapeDragging() {
   configureMouseDownEventListener();
-  configureMouseDragEventListener();
+  configureMouseMoveEventListener();
   configureMouseUpEventListener();
 }
 
-// Offset X/Y will return the mouse click cooridnates relative to the canvas, rather then the whole screen.
-// When a createShapeFunction is present, the user has pressed a button to create a shape, then when he goes to the canvas
-// and presses his mouse in order to start dragging, it will trigger this function.
 function configureMouseDownEventListener() {
   canvas.addEventListener('mousedown', (event) => {
-    if (!createShapeFunction) {
-      return;
+    if (dragFunctions) {
+      dragFunctions.onMouseDown.call(this, event);
     }
-    startX = event.offsetX;
-    startY = event.offsetY;
-    isDragging = true;
-    shape = createShapeFunction.call(
-      this,
-      canvas,
-      {
-        x: event.offsetX,
-        y: event.offsetY,
-      },
-      {
-        width: 0,
-        height: 0,
-      }
-    );
   });
 }
 
-// The X and Y attributes of an SVG element point to the top left corner, so if the user drags from the upper left corner to the bottom right one
-// there will be no issue, the shape will be created as expected.
-// If the user drags in any other direction, he will not get the desired outcome by default.
-// In order to support dragging on all directions, the function will calculate whether the X and Y need to move to accomate the direction of the drag.
-function configureMouseDragEventListener() {
+function configureMouseMoveEventListener() {
   canvas.addEventListener('mousemove', (event) => {
-    if (isDragging) {
-      const currentX = event.offsetX;
-      const currentY = event.offsetY;
-
-      const width = Math.abs(currentX - startX);
-      const height = Math.abs(currentY - startY);
-
-      // If currentX < startX -> the drag is left, otherwise right.
-      const shapeX = currentX < startX ? currentX : startX;
-      // If currentY < startY -> the drag is up, otherwise down.
-      const shapeY = currentY < startY ? currentY : startY;
-
-      shape.setAttribute('width', width);
-      shape.setAttribute('height', height);
-      shape.setAttribute('x', shapeX);
-      shape.setAttribute('y', shapeY);
+    if (dragFunctions) {
+      dragFunctions.onMouseMove.call(this, event);
     }
   });
 }
 
-// When the mouse button has been released, it means the user has finished drawing the shape
-// therefor all the state for that specific shape is cleared.
 function configureMouseUpEventListener() {
   canvas.addEventListener('mouseup', () => {
-    shape = null;
-    startX = null;
-    startY = null;
-    isDragging = false;
+    if (dragFunctions) {
+      dragFunctions.onMouseUp.call(this);
+    }
   });
 }
---
+++ scripts/shape-drag-sizing/square-drag.js
@@ -0,0 +1,60 @@
+import appendSquareToCanvas from '../shape-factory/square-factory.js';
+
+let shape, startX, startY;
+
+let isDragging = false;
+
+// Offset X/Y will return the mouse click cooridnates relative to the canvas, rather then the whole screen.
+// When a createShapeFunction is present, the user has pressed a button to create a shape, then when he goes to the canvas
+// and presses his mouse in order to start dragging, it will trigger this function.
+export function createSquare(event) {
+  startX = event.offsetX;
+  startY = event.offsetY;
+  isDragging = true;
+  shape = appendSquareToCanvas(
+    canvas,
+    {
+      x: event.offsetX,
+      y: event.offsetY,
+    },
+    {
+      width: 0,
+      height: 0,
+    }
+  );
+}
+
+// The X and Y attributes of an SVG element point to the top left corner, so if the user drags from the upper left corner to the bottom right one
+// there will be no issue, the shape will be created as expected.
+// If the user drags in any other direction, he will not get the desired outcome by default.
+// In order to support dragging on all directions, the function will calculate whether the X and Y need to move to accomate the direction of the drag.
+export function resizeSquareWhileDragging(event) {
+  if (!isDragging) {
+    return;
+  }
+
+  const currentX = event.offsetX;
+  const currentY = event.offsetY;
+
+  const width = Math.abs(currentX - startX);
+  const height = Math.abs(currentY - startY);
+
+  // If currentX < startX -> the drag is left, otherwise right.
+  const shapeX = currentX < startX ? currentX : startX;
+  // If currentY < startY -> the drag is up, otherwise down.
+  const shapeY = currentY < startY ? currentY : startY;
+
+  shape.setAttribute('width', width);
+  shape.setAttribute('height', height);
+  shape.setAttribute('x', shapeX);
+  shape.setAttribute('y', shapeY);
+}
+
+// When the mouse button has been released, it means the user has finished drawing the shape
+// therefor all the state for that specific shape is cleared.
+export function resetDragState() {
+  shape = null;
+  startX = null;
+  startY = null;
+  isDragging = false;
+}
