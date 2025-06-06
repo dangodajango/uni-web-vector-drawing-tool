b5863a815fe9e74e191c466c110f97f748ea8271
stu2101321071 <stu2101321071@uni-plovdiv.bg>
2025-06-06 13:17:03 +0300

    Implement shape

--- scripts/drawing-tools/drawing-tools-configuration.js
+++ scripts/drawing-tools/drawing-tools-configuration.js
@@ -24,14 +24,14 @@ function configureEllipseButton() {
 }
 
 function configureShape1Button() {
-  // const shape1Button = document.getElementById('extra-shape');
-  // shape1Button.addEventListener('click', () => {
-  //   const canvas = document.getElementById('canvas');
-  //   canvas.innerHTML = `<path
-  //       id="shape1"
-  //       d="M158 108m-50 0a50 50 0 10100 0 50 50 0 10-100 0M208 108 208 108M197 76 119 139M120 75 197 140"
-  //       stroke="black"
-  //       fill="none"
-  //       stroke-width="2"/>`
-  // });
+  const shape1Button = document.getElementById('extra-shape');
+  shape1Button.addEventListener('click', () => {
+    const canvas = document.getElementById('canvas');
+    canvas.innerHTML = `<path
+        id="shape1"
+        d="M158 108m-50 0a50 50 0 10100 0 50 50 0 10-100 0M208 108 208 108M197 76 119 139M120 75 197 140"
+        stroke="black"
+        fill="none"
+        stroke-width="2"/>`
+  });
 }
